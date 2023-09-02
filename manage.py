92726b5a9ab0cdd319feec29114a24b4ad3f82b3
tienv2i <tienv2i@gmail.com>
2023-09-02 20:18:51 +0700

    First commit @

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
+
+dotenv.load_dotenv()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mii.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miiblog.settings")
 
     from django.core.management import execute_from_command_line
 
--- miiblog/wsgi.py
+++ miiblog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mii project.
+WSGI config for miiblog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mii.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miiblog.settings")
 
 application = get_wsgi_application()
