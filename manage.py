58112b989644c4267b9b1e93b4e3248892136447
tienv2i <tienv2i@gmail.com>
2023-09-03 09:27:35 +0700

    Second

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
+dotenv.load_dotenv()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miiblog.settings.dev")
